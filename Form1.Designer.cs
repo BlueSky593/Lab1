8f60113da1a6f315151a20bbdb3e97fa37073a78
BlueSky593 <david.mcdon@protonmail.com>
2021-04-22 20:53:15 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPlaceOrder = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtnSalad = new System.Windows.Forms.RadioButton();
+            this.radioBtnPizza = new System.Windows.Forms.RadioButton();
+            this.radioBtnHamb = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrderTot = new System.Windows.Forms.TextBox();
+            this.txtTax = new System.Windows.Forms.TextBox();
+            this.txtSubTot = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnPlaceOrder
+            // 
+            resources.ApplyResources(this.btnPlaceOrder, "btnPlaceOrder");
+            this.btnPlaceOrder.Name = "btnPlaceOrder";
+            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
+            // 
+            // btnReset
+            // 
+            resources.ApplyResources(this.btnReset, "btnReset");
+            this.btnReset.Name = "btnReset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClose
+            // 
+            resources.ApplyResources(this.btnClose, "btnClose");
+            this.btnClose.Name = "btnClose";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.radioBtnSalad);
+            this.groupBox1.Controls.Add(this.radioBtnPizza);
+            this.groupBox1.Controls.Add(this.radioBtnHamb);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // radioBtnSalad
+            // 
+            resources.ApplyResources(this.radioBtnSalad, "radioBtnSalad");
+            this.radioBtnSalad.Name = "radioBtnSalad";
+            this.radioBtnSalad.UseVisualStyleBackColor = true;
+            this.radioBtnSalad.CheckedChanged += new System.EventHandler(this.radioBtnSalad_CheckedChanged);
+            // 
+            // radioBtnPizza
+            // 
+            resources.ApplyResources(this.radioBtnPizza, "radioBtnPizza");
+            this.radioBtnPizza.Name = "radioBtnPizza";
+            this.radioBtnPizza.UseVisualStyleBackColor = true;
+            this.radioBtnPizza.CheckedChanged += new System.EventHandler(this.radioBtnPizza_CheckedChanged);
+            // 
+            // radioBtnHamb
+            // 
+            resources.ApplyResources(this.radioBtnHamb, "radioBtnHamb");
+            this.radioBtnHamb.Name = "radioBtnHamb";
+            this.radioBtnHamb.UseVisualStyleBackColor = true;
+            this.radioBtnHamb.CheckedChanged += new System.EventHandler(this.radioBtnHamb_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Controls.Add(this.checkBox3);
+            this.groupBox2.Controls.Add(this.checkBox2);
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // checkBox3
+            // 
+            resources.ApplyResources(this.checkBox3, "checkBox3");
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            resources.ApplyResources(this.groupBox3, "groupBox3");
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.txtOrderTot);
+            this.groupBox3.Controls.Add(this.txtTax);
+            this.groupBox3.Controls.Add(this.txtSubTot);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // txtOrderTot
+            // 
+            resources.ApplyResources(this.txtOrderTot, "txtOrderTot");
+            this.txtOrderTot.Name = "txtOrderTot";
+            // 
+            // txtTax
+            // 
+            resources.ApplyResources(this.txtTax, "txtTax");
+            this.txtTax.Name = "txtTax";
+            // 
+            // txtSubTot
+            // 
+            resources.ApplyResources(this.txtSubTot, "txtSubTot");
+            this.txtSubTot.Name = "txtSubTot";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnPlaceOrder;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.CancelButton = this.btnReset;
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnPlaceOrder);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlaceOrder;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtnSalad;
+        private System.Windows.Forms.RadioButton radioBtnPizza;
+        private System.Windows.Forms.RadioButton radioBtnHamb;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrderTot;
+        private System.Windows.Forms.TextBox txtTax;
+        private System.Windows.Forms.TextBox txtSubTot;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //public constant declaration and assignments
+        public const double HamburgerPrice = 6.95;
+        public const double PizzaPrice = 5.95;
+        public const double SaladPrice = 4.95;
+        //public variable declarations
+        public double mcPrice;
+        public double addOnPrice;
+
+        // at the start 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            radioBtnHamb.Checked = true;
+            checkBox1.Text = "Lettuce, tomato, and onions";
+            checkBox2.Text = "Ketchup, mustard, and mayo";
+            checkBox3.Text = "French fries";
+            groupBox2.Text = "Add - on items($.75/each)";
+        }
+
+        //radio buttons
+        private void radioBtnHamb_CheckedChanged(object sender, EventArgs e)
+        {
+            //assignments
+            mcPrice = HamburgerPrice;
+            addOnPrice = .75;
+
+            groupBox2.Text = "Add - on items($.75/each)";
+            checkBox1.Text = "Lettuce, tomato, and onions";
+            checkBox2.Text = "Ketchup, mustard, and mayo";
+            checkBox3.Text = "French fries";
+        }
+
+        private void radioBtnPizza_CheckedChanged(object sender, EventArgs e)
+        {
+            //assignments
+            mcPrice = PizzaPrice;
+            addOnPrice = .50;
+
+            groupBox2.Text = "Add - on items($.50/each)";
+            checkBox1.Text = "Pepperoni";
+            checkBox2.Text = "Sausage";
+            checkBox3.Text = "Olives";
+        }
+
+        private void radioBtnSalad_CheckedChanged(object sender, EventArgs e)
+        {
+            //assignments
+            mcPrice = SaladPrice;
+            addOnPrice = .25;
+
+            groupBox2.Text = "Add - on items($.25/each)";
+            checkBox1.Text = "Croutons";
+            checkBox2.Text = "Bacon bits";
+            checkBox3.Text = "Bread sticks";
+        }
+
+
+        //Place Order Button
+        private void btnPlaceOrder_Click(object sender, EventArgs e)
+        {
+            //local declarations
+            double payAmount;
+            double addOnPriceTot;
+            double Tax;
+            int addOnNum;
+            double Subtotal;
+
+            // addOnNum assignment         
+            if (checkBox1.Checked == true & checkBox2.Checked == false & checkBox3.Checked == false | checkBox1.Checked == false & 
+                checkBox2.Checked == true & checkBox3.Checked == false | checkBox1.Checked == false & checkBox2.Checked == false & checkBox3.Checked == true)
+                addOnNum = 1;
+            else if (checkBox1.Checked == true & checkBox2.Checked == true & checkBox3.Checked == false | checkBox1.Checked == true & checkBox2.Checked == false 
+                & checkBox3.Checked == true | checkBox1.Checked == false & checkBox2.Checked == true & checkBox3.Checked == true)
+                addOnNum = 2;
+            else if (checkBox1.Checked == true & checkBox2.Checked == true & checkBox3.Checked == true)
+                addOnNum = 3;
+            else
+                addOnNum = 0;
+
+            //add on price total calculation
+            addOnPriceTot = addOnPrice * addOnNum;
+
+            //subtotal assignment/calculation
+            Subtotal = mcPrice + addOnPriceTot;
+
+            //tax calculation
+            Tax = 0.05 * (mcPrice + addOnPriceTot);
+
+            //Total price calculation
+            payAmount = mcPrice + addOnPriceTot + Tax;
+
+            //Display Output
+            txtSubTot.Text = Subtotal.ToString("c"); // currency format
+            txtTax.Text = Tax.ToString("c"); // currency format
+            txtOrderTot.Text = payAmount.ToString("c"); // currency format
+
+        }
+        //reset button
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            radioBtnHamb.Checked = true;
+            txtSubTot.Text = "";
+            txtTax.Text = "";
+            txtOrderTot.Text = "";
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+
+        }
+
+        //Exit button
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
